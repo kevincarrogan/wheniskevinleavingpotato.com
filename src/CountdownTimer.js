c30da72e8f73b81f3b8f20ddc4321eb0e1bf5c78
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-14 23:45:59 +0000

    Tweak styling of countdown timer and only display relevant units.

--- src/CountdownTimer.js
+++ src/CountdownTimer.js
@@ -4,6 +4,10 @@ import { DateTime } from 'luxon';
 
 import FixedWidthText from './FixedWidthText';
 
+const Fixed = ({ t }) => (
+  <FixedWidthText text={t} width="90px" />
+);
+
 class CountdownTimer extends Component {
   onAnimationFrame() {
     this.forceUpdate();
@@ -12,11 +16,24 @@ class CountdownTimer extends Component {
   render() {
     const diff = DateTime
                   .fromJSDate(this.props.until)
-                  .diffNow()
-                  .toFormat('hh:mm:ss.SSS');
+                  .diffNow();
+
+    let [hh, mm, ss, sss] = diff.toFormat('hh:mm:ss:SSS').split(':');
+
     return (
       <div className="countdown-timer">
-        <FixedWidthText text={diff} width="45px" />
+        { hh > 0 &&
+          <>
+            <Fixed t={hh} />:
+          </>
+        }
+        {
+          mm > 0 &&
+          <>
+            <Fixed t={mm} />:
+          </>
+        }
+        <Fixed t={ss} />.<Fixed t={sss} />
       </div>
     );    
   }
