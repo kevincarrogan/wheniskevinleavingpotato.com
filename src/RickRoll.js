2cf15ce169354480c10e3b9d8018217a0078220b
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-15 12:47:05 +0000

    Add actual rickroll embed.

--- src/RickRoll.js
+++ src/RickRoll.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+
 const RickRoll = () => (
-  'RICK LOL'
+  <YouTube
+    videoId="DLzxrzFCyOs"
+    opts={{
+      width: 800,
+      height: 600,
+      playerVars: {
+        autoplay: 1,
+        controls: 0,
+        playsinline: 1,
+        loop: 1
+      }
+    }}
+  />
 );
 
 export default RickRoll;
