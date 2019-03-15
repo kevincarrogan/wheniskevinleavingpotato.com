d06f8e476812ac827d9b23758b61c498d3117dcf
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-15 11:58:12 +0000

    Change app from class based component to function based.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import CountdownTimer from './CountdownTimer';
 import Logo from './Logo';
@@ -6,21 +6,19 @@ import AnimateLogo from './AnimateLogo';
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    const leavingTime = new Date(2019, 2, 29, 17);
-    return (
-      <main>
-        <h1>When is Kevin leaving Potato?</h1>
-        <CountdownTimer until={leavingTime} />
-        <AnimateLogo leavingTime={leavingTime}>
-          {scale => (
-            <Logo scale={scale} />
-          )}
-        </AnimateLogo>
-      </main>
-    );
-  }
+const App = () => {
+  const leavingTime = new Date(2019, 2, 29, 17);
+  return (
+    <main>
+      <h1>When is Kevin leaving Potato?</h1>
+      <CountdownTimer until={leavingTime} />
+      <AnimateLogo leavingTime={leavingTime}>
+        {scale => (
+          <Logo scale={scale} />
+        )}
+      </AnimateLogo>
+    </main>
+  );
 }
 
 export default App;
