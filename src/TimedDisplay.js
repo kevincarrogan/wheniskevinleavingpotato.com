61bcdedda58df3426b26bb2bdcfae8fac5e295a5
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-15 14:06:36 +0000

    Timed display components update in real time.

--- src/TimedDisplay.js
+++ src/TimedDisplay.js
@@ -1,15 +1,52 @@
-const DisplayBefore = ({ when, children }) => {
-  if (new Date() <= when) {
-    return children;
-  }
-  return null;
-};
+import { Component } from 'react';
+import ReactAnimationFrame from 'react-animation-frame';
 
-const DisplayAfter = ({ when, children }) => {
-  if (new Date() > when) {
-    return children;
+class DisplayBefore extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {display: this.shouldDisplay(new Date())}
   }
-  return null;
-};
+
+  shouldDisplay(date) {
+    return date <= this.props.when;
+  }
+
+  onAnimationFrame() {
+    this.setState({display: this.shouldDisplay(new Date())});
+  }
+
+  render() {
+    if (this.state.display) {
+      return this.props.children;
+    }
+
+    return null;
+  }
+}
+DisplayBefore = ReactAnimationFrame(DisplayBefore);
+
+class DisplayAfter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {display: this.shouldDisplay(new Date())}
+  }
+
+  shouldDisplay(date) {
+    return date > this.props.when;
+  }
+
+  onAnimationFrame() {
+    this.setState({display: this.shouldDisplay(new Date())});
+  }
+
+  render() {
+    if (this.state.display) {
+      return this.props.children;
+    }
+
+    return null;
+  }
+}
+DisplayAfter = ReactAnimationFrame(DisplayAfter);
 
 export { DisplayBefore, DisplayAfter };
