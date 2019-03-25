af72f2e3724bd9ef54e3ed969215a1b574b02089
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-25 22:50:37 +0000

    Swap hook based ship component for class based.

--- src/Ship.js
+++ src/Ship.js
@@ -1,13 +1,50 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 import Logo from './Logo';
 
-const Ship = ({ tick }) => {
-  const rotate = tick % 360;
+class Ship extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Logo width="180" height="186" rotate={rotate} scale="1" opacity="1" />
-  );
-};
+    this.state = {
+      rotation: 0,
+      lastTick: props.tick,
+      direction: 1,
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    const diff = (props.tick - state.lastTick) * state.direction;
+    const rotation = (state.rotation + diff) % 360;
+
+    return {
+      lastTick: props.tick,
+      rotation,
+    };
+  }
+
+  onClick() {
+    this.setState((state, props) => ({
+      ...state,
+      direction: state.direction * -1,
+    }));
+  }
+
+  render() {
+    return (
+      <div onClick={this.onClick}>
+        <Logo
+          width="180"
+          height="186"
+          rotate={this.state.rotation}
+          scale="1"
+          opacity="1"
+        />
+      </div>
+    );
+  }
+}
 
 export default Ship;
