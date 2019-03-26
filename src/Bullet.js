ddbef07017cdde823dfdac1fbb878adfe4c29161
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-26 13:21:07 +0000

    Move bullet state logic into game component from bullet component.

--- src/Bullet.js
+++ src/Bullet.js
@@ -1,63 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-const toRadians = angle => angle * (Math.PI / 180);
-const velocity = 5;
+const Bullet = ({ x, y }) => {
+  const left = 295 - x;
+  const top = 295 - y;
 
-class Bullet extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      lastTick: props.tick,
-      movement: 45,
-    };
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    const diff = props.tick - state.lastTick;
-    const movement = state.movement + diff * velocity;
-
-    const rotation = toRadians(props.rotation);
-    const x = state.movement * Math.cos(rotation + Math.PI * 1.5);
-    const y = state.movement * Math.sin(rotation + Math.PI * 1.5);
-
-    const left = 295 - x;
-    const top = 295 - y;
-
-    return {
-      lastTick: props.tick,
-      movement,
-      left,
-      top,
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.top < 0 ||
-      prevState.top > 600 ||
-      prevState.left < 0 ||
-      prevState.left > 600
-    ) {
-      prevProps.remove(prevProps.id);
-    }
-  }
-
-  render() {
-    return (
-      <div
-        style={{
-          backgroundColor: `white`,
-          borderRadius: `50%`,
-          height: `10px`,
-          left: `${this.state.left}px`,
-          position: `absolute`,
-          top: `${this.state.top}px`,
-          width: `10px`,
-        }}
-      />
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        backgroundColor: `white`,
+        borderRadius: `50%`,
+        height: `10px`,
+        left: `${left}px`,
+        position: `absolute`,
+        top: `${top}px`,
+        width: `10px`,
+      }}
+    />
+  );
+};
 
 export default Bullet;
--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactAnimationFrame from 'react-animation-frame';
+import uuidv4 from 'uuid/v4';
 
 import Ship from './Ship';
 import Bullet from './Bullet';
@@ -9,6 +10,8 @@ import styles from './Game.module.css';
 
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (+max - +min)) + +min;
+
+const toRadians = angle => angle * (Math.PI / 180);
 
 class Game extends Component {
   constructor(props) {
@@ -21,10 +24,12 @@ class Game extends Component {
 
     this.fire = this.fire.bind(this);
     this.remove = this.remove.bind(this);
+    this.initBullet = this.initBullet.bind(this);
+    this.updateBullets = this.updateBullets.bind(this);
 
     this.state = {
       tick: 0,
-      bullets: {},
+      bullets: [],
       enemies: {},
       enemySpawnTimer: 0,
     };
@@ -34,9 +39,14 @@ class Game extends Component {
     const diff = timestamp - lastTimestamp;
     const tick = Math.ceil(diff / this._interval);
 
-    this.setState((state, props) => ({
-      tick: state.tick + tick,
-    }));
+    this.setState((state, props) => {
+      const newTick = state.tick + tick;
+      const bullets = this.updateBullets(state.bullets, newTick);
+      return {
+        tick: newTick,
+        bullets,
+      };
+    });
 
     if (this.state.enemySpawnTimer > this.enemySpawnRate) {
       this.addEnemy(randomInt(0, 360));
@@ -48,9 +58,52 @@ class Game extends Component {
     }
   }
 
+  initBullet(rotation) {
+    const id = uuidv4();
+    const movement = 45;
+    const radians = toRadians(rotation);
+    const x = movement * Math.cos(radians + Math.PI * 1.5);
+    const y = movement * Math.sin(radians + Math.PI * 1.5);
+    return {
+      lastTick: this.state.tick,
+      id,
+      rotation,
+      movement,
+      x,
+      y,
+    };
+  }
+
+  updateBullets(bullets, tick) {
+    const velocity = 5;
+
+    if (bullets.length === 0) {
+      return bullets;
+    }
+
+    return bullets.map(bullet => {
+      const diff = tick - bullet.lastTick;
+      const movement = bullet.movement + diff * velocity;
+      const radians = toRadians(bullet.rotation);
+      const x = movement * Math.cos(radians + Math.PI * 1.5);
+      const y = movement * Math.sin(radians + Math.PI * 1.5);
+      const lastTick = tick;
+      const updatedBullet = {
+        ...bullet,
+        movement,
+        lastTick,
+        x,
+        y,
+      };
+      return updatedBullet;
+    });
+  }
+
   fire(rotation) {
+    const bullet = this.initBullet(rotation);
+
     this.setState((state, props) => ({
-      bullets: { ...this.state.bullets, [state.tick]: rotation },
+      bullets: [...this.state.bullets, bullet],
     }));
   }
 
@@ -71,14 +124,8 @@ class Game extends Component {
     return (
       <div className={styles.game}>
         <Ship tick={this.state.tick} fire={this.fire} />
-        {Object.entries(this.state.bullets).map(([id, rotation]) => (
-          <Bullet
-            id={id}
-            key={id}
-            remove={this.remove}
-            tick={this.state.tick}
-            rotation={rotation}
-          />
+        {this.state.bullets.map(bullet => (
+          <Bullet key={bullet.id} x={bullet.x} y={bullet.y} />
         ))}
         {Object.entries(this.state.enemies).map(([id, rotation]) => (
           <Enemy id={id} key={id} rotation={rotation} tick={this.state.tick} />
