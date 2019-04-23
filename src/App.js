c4a1e1a30344392b6411e7538b8c866c4be41b31
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-04-23 09:02:01 +0100

    Stop rickrolling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
 
-import CountdownTimer from './CountdownTimer';
-import RickRoll from './RickRoll';
-import { DisplayBefore, DisplayAfter } from './TimedDisplay';
-import TimedAudio from './TimedAudio';
 import Game from './Game';
 
 import './App.css';
 
-import sunrise from './sunrise.mp3';
-
 const App = () => {
-  const leavingTime = new Date(2019, 2, 29, 16, 30);
   return (
     <main>
       <h1>When is Kevin leaving Potato?</h1>
-      <DisplayBefore when={leavingTime}>
-        <CountdownTimer until={leavingTime} />
-        <Game />
-      </DisplayBefore>
-      <DisplayAfter when={leavingTime}>
-        <RickRoll />
-      </DisplayAfter>
-      <TimedAudio src={sunrise} finish={leavingTime} />
+      <Game />
     </main>
   );
 };
